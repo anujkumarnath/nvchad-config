c17ee3dc73de3c1a644e658a8b7a21b25994171a
Anuj <anuj@antares-tech.com>
2024-04-21 19:50:21 +0000

    added floating term mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Alt + i in normal or terminal mode to open floating terminal
+map({ "n", "t" }, "<A-i>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "Terminal Toggle Floating term" })
